3466352cf44e471a4d36f7212cb385b16f8e1852
chen yujian <chen.yujian@cesgroup.com.cn>
2016-01-22 10:11:14 +0800

    baidu tongji

---
+++ analysis.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?9ba0a85c123b7847ce15b80f4ffd78ee";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
